ccee569b0933acb11606d5c82a9a656cc9726cc9
med1337 <leszek.bagnucki@gmail.com>
2017-09-29 15:00:23 +0100

    truck driving

---
+++ Assets/Axis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    public bool Steering;
+    public bool Motor;
+
+
+    public WheelCollider LeftCollider;
+    public WheelCollider RightCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+      
+    }
+}
---
+++ Assets/WheelControlScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelControlScript : MonoBehaviour
+{
+
+    public float motor;
+    public float steering;
+
+    public float SteeringAngle;
+
+    public float MotorTorque;
+    public List<Axis> AxlesList;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        motor = MotorTorque * Input.GetAxis("Vertical");
+        steering = SteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (Axis axleInfo in AxlesList)
+        {
+            if (axleInfo.Steering)
+            {
+                axleInfo.LeftCollider.steerAngle = steering;
+                axleInfo.RightCollider.steerAngle = steering;
+            }
+            if (axleInfo.Motor)
+            {
+                axleInfo.LeftCollider.motorTorque = motor;
+                axleInfo.RightCollider.motorTorque = motor;
+            }
+        }
+    }
+}
