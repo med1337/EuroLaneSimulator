4ee94023b5f2c87a24b89a9df549df1447f0922c
med1337 <leszek.bagnucki@gmail.com>
2017-09-29 16:10:23 +0100

    truck ftw

---
+++ Assets/RigidbodyAdjustment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyAdjustment : MonoBehaviour
+{
+
+    public Vector3 RbadjustVector3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    GetComponent<Rigidbody>().centerOfMass = RbadjustVector3;
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 com = GetComponent<Rigidbody>().centerOfMass;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.TransformPoint(com), 0.15f);
+    }
+}
