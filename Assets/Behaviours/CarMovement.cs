ee8661caad2fa0541413a0150ecb3bb83a08b6ec
Elliott Smith <elliott2.smith@live.uwe.ac.uk>
2017-09-29 13:30:02 +0100

    car obstacle

---
+++ Assets/Behaviours/CarMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+    public int speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //move car
+        transform.Translate(0, Time.deltaTime * speed, 0);
+    }
+}
