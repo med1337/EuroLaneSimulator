4f270170895255cd3946776dcf550b8c58c578ad
Elliott Smith <elliott2.smith@live.uwe.ac.uk>
2017-09-29 14:00:42 +0100

    script update

--- Assets/Behaviours/ObstacleManagerScript.cs
+++ Assets/Behaviours/ObstacleManagerScript.cs
@@ -10,8 +10,8 @@ public class ObstacleManagerScript : MonoBehaviour
     private int currentObstacles = 0; //how many obstacles spawned
     private int minSpawnTime = 1; //minimum time to next spawn
     private int maxSpawnTime = 5; //maximum time to next spawn
-    private float spawnDistance = 10.0f; //distance obstacles spawn/despawn fomr player
-    private List<GameObject> obstacles = new List<GameObject>(); //obstacle List
+    private float spawnDistance = 5.0f; //distance obstacles spawn/despawn fomr player
+    public List<GameObject> obstacles = new List<GameObject>(); //obstacle List
     public GameObject obstacleOne; //obstacle GO to spawn
 
     // Use this for initialization
@@ -25,12 +25,12 @@ public class ObstacleManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if there are obstacles far enough behind player, despawn
+        //if there are obstacles far enough behind or in front of player, despawn
         if (currentObstacles > 0)
         {
             foreach (GameObject obstacle in obstacles)
             {
-                if (obstacle.transform.position.y > player.transform.position.y - spawnDistance)
+                if ((obstacle.transform.position.y < player.transform.position.y - spawnDistance) || (obstacle.transform.position.y > player.transform.position.y + spawnDistance * 2))
                 {
                     obstacles.Remove(obstacle);
 
