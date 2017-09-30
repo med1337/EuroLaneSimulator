fae731aa4040277674e9790d2c63055d59c5de3d
Joe da Silva <silvacore@hotmail.co.uk>
2017-09-30 15:55:45 +0100

    simple UI transaction display system (viewable in DamageSystem scene)

---
+++ Assets/Behaviours/UI/MoneyPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyPanel : MonoBehaviour
+{
+    public int money;
+
+    [Header("Parameters")]
+    [SerializeField] GameObject transaction_prefab;
+    [SerializeField] float transaction_fade_delay;
+    [SerializeField] float transaction_fade_duration;
+
+    [Header("References")]
+    [SerializeField] Text money_display;
+    [SerializeField] Transform grid_transform;
+
+
+    public void LogTransaction(int _amount, string _note)
+    {
+        GameObject clone = Instantiate(transaction_prefab, grid_transform);
+        TransactionUIElement t = clone.GetComponent<TransactionUIElement>();
+
+        money += _amount;
+        money_display.text = money.ToString();
+
+        t.Init(_amount, _note, transaction_fade_delay, transaction_fade_duration);
+        t.transform.SetAsFirstSibling();
+    }
+
+}
---
+++ Assets/Editor/MoneyPanelEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MoneyPanel))]
+public class MoneyPanelEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("$100 Cash Bonus"))
+        {
+            if (!EditorApplication.isPlaying)
+                return;
+
+            var t = (MoneyPanel)target;
+            t.LogTransaction(100, "Debug Cash Bonus");
+        }
+
+        if (GUILayout.Button("$100 Cash Penalty"))
+        {
+            if (!EditorApplication.isPlaying)
+                return;
+
+            var t = (MoneyPanel)target;
+            t.LogTransaction(-100, "Debug Cash Penalty");
+        }
+    }
+
+}
