233ac9e842f61146b963a5a23a466dc28f39c017
Tom Turner <tom.turner2@googlemail.com>
2017-09-30 21:52:46 +0100

    new assets

---
+++ Assets/Behaviours/ObjectiveManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    private static GameObject[] depots;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    depots = GameObject.FindGameObjectsWithTag("Depot");
+	}
+	
+}
