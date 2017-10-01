7796c805813a92d296a658abb12ba801a1039ef3
Joe da Silva <silvacore@hotmail.co.uk>
2017-10-01 19:36:28 +0100

    the follow camera now leads the truck and zooms out based on speed

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    [SerializeField] Camera cam;
+    [SerializeField] Truck truck;
+    [SerializeField] float base_zoom = 29.09f;
+    [SerializeField] float additional_zoom = 5;
+    [SerializeField] float lead_factor;
 
-    public Transform GameObjecTransform;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,8 +19,10 @@ public class FollowCamera : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        Vector3 x = new Vector3(0,20,0);
-	    transform.position = GameObjecTransform.position + x;
+        Vector3 pos = new Vector3(0,20,0) + truck.transform.position;
+        transform.position = lead_factor == 0 ? pos : pos + (truck.transform.forward * truck.Speed * lead_factor);
 
+	    cam.orthographicSize = base_zoom + ((truck.Speed / truck.SpeedLimit) * additional_zoom);
+	    cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, base_zoom, base_zoom + additional_zoom);
 	}
 }
