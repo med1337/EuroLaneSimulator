c21fcb442f41a2a93fc08092ca52df69c9c7f8d7
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-10-03 12:38:41 +0000

    Arrow Added to Truck

---
+++ Assets/City Generation/ArrowLookAtTarget.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ArrowLookAtTarget : MonoBehaviour
+{
+    private GameObject target;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // If we have a valid target, uppdate
+        if (target != null)
+        {
+            transform.LookAt(target.transform);
+        }
+	}
+
+
+    public void DisableArrow()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+    public void EnableArrow()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+
+    public void SetTarget(GameObject new_target)
+    {
+        target = new_target;
+    }
+}
